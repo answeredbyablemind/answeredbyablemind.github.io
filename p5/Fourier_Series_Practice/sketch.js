eea0f7c641212d089362026032090eadb4ccf82a
angeloyeo <angeloyeo@gmail.com>
2019-06-23 09:23:59 +0900

    Fourier Series p5.js

--- p5/Fourier_Series_Practice/sketch.js
+++ p5/Fourier_Series_Practice/sketch.js
@@ -1,38 +1,50 @@
 let time = 0;
 let wave = [];
-
+let slider;
 function setup() {
      createCanvas(600, 400);
+     slider = createSlider(1,10,5);
 }
 
 function draw() {
      background(0)
-     translate(200, 200)
+     translate(100, 200)
 
-     stroke(255)
-     noFill()
+     let x = 0
+     let y = 0
 
-     let radius = 100 * (4 / (1 * PI));
-     ellipse(0, 0, radius * 2)
-     let x = radius * cos(1 * time)
-     let y = radius * sin(1 * time)
+     for (i = 0; i < slider.value(); i++) {
+          let prevx = x;
+          let prevy = y;
+
+          let n = i * 2 + 1
+          let radius = 50 * (4 / (n * PI));
+          x += radius * cos(n * time)
+          y += radius * sin(n * time)
+
+          stroke(255,100)
+          noFill()
+          ellipse(prevx, prevy, radius * 2)
+
+          stroke(255)
+          fill(255)
+          line(prevx, prevy, x, y)
+     }
      wave.unshift(y)
-     fill(255)
-     line(0, 0, x, y)
-     ellipse(x, y, 8);
 
      translate(200, 0)
      line(x - 200, y, 0, wave[0])
      noFill()
+
      beginShape()
      for (let i = 0; i < wave.length; i++) {
           vertex(i, wave[i]);
      }
      endShape();
-     // aa
+
      time += 0.05;
 
-     if (wave.length > 250) {
+     if (wave.length > 260) {
           wave.pop()
      }
 }
