0dfb560c8c52b0293b1211ff7c960c30d1e3eb01
angeloyeo <angeloyeo@gmail.com>
2019-07-01 15:25:50 +0900

    -

--- p5/DFT_Practice/sketch.js
+++ p5/DFT_Practice/sketch.js
@@ -1,37 +1,45 @@
 let time = 0;
 let wave = [];
-let y = [];
-let fourierY;
-
+let y =[];
+let Four
 
 function setup() {
      createCanvas(600, 400);
-     y = [100, 100, 100, -100, -100, -100, 100, 100, 100, -100, -100, -100, 100, 100, 100, -100, -100, -100]
-     fourierY = dft(y);
-     console.log(fourierY)
+     slider = createSlider(1,10,5);
+     slider.position(20, height-30)
 }
 
 function draw() {
      background(0)
+     fill(255)
+     textAlign(RIGHT,CENTER)
+     text("(c) 공돌이의 수학정리노트",575,375)
+
+     textAlign(CENTER, CENTER)
+     textSize(15)
+     text("아래의 슬라이더를 움직여보자", width/2, 75)
+     translate(100, 200)
+
+
      let x = 0
      let y = 0
 
-     for (i = 0; i < fourierY.length; i++) {
+     for (i = 0; i < slider.value(); i++) {
           let prevx = x;
           let prevy = y;
-          //
-          // let n = i * 2 + 1
-          // let radius = 50 * (4 / (n * PI));
-          // x += radius * cos(n * time)
-          // y += radius * sin(n * time)
-          //
-          // stroke(255, 100)
-          // noFill()
-          // ellipse(prevx, prevy, radius * 2)
-          //
-          // stroke(255)
-          // fill(255)
-          // line(prevx, prevy, x, y)
+
+          let n = i * 2 + 1
+          let radius = 50 * (4 / (n * PI));
+          x += radius * cos(n * time)
+          y += radius * sin(n * time)
+
+          stroke(255,100)
+          noFill()
+          ellipse(prevx, prevy, radius * 2)
+
+          stroke(255)
+          fill(255)
+          line(prevx, prevy, x, y)
      }
      wave.unshift(y)
 
