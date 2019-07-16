60b8bdc68c9ccd9da749cb783de0fc092825ff0d
angeloyeo <angeloyeo@gmail.com>
2019-07-16 10:07:05 +0900

    slider, matrix 설정

--- p5/Matrix_as_a_linear_transformation/transformation1/sketch.js
+++ p5/Matrix_as_a_linear_transformation/transformation1/sketch.js
@@ -3,10 +3,12 @@ let vu = [] // vertical line의 upper point에 해당하는 vector
 let vd = []
 let hl = []
 let hr = []
+let slider1;
+let myMtx = [];
 
 function setup() {
      // createCanvas(windowWidth - 20, windowHeight - 20);
-     createCanvas(800,400);
+     createCanvas(800, 400);
      scl = floor(height / 8); // height가 낮다보니 height에 scale을 맞추는게 좋아보임.
 
      // 각 아래의 내용을 vu, vd, hl, hr에 맞출 수 있게... 벡터화 하고자 함.
@@ -14,16 +16,38 @@ function setup() {
      // vertical line에 해당하는 vectors
      for (let i = -floor(width / scl) / 2; i < floor(width / scl) / 2; i++) {
           // line(i * scl, -height / 2, i * scl, height / 2)
-          vu.push([[i], [height / (2*scl)]]);
-          vd.push([[i], [-height / (2*scl)]]);
+          vu.push([
+               [i],
+               [height / (2 * scl)]
+          ]);
+          vd.push([
+               [i],
+               [-height / (2 * scl)]
+          ]);
      }
      // horizontal line에 해당하는 vectors
      for (let i = -floor(height / scl) / 2; i < floor(height / scl) / 2; i++) {
           // line(-width / 2, i * scl, width / 2, i * scl)
           // line(x1, y1, x2, y2) 형식으로 그려짐
-          hl.push([[- width/(2*scl)], [i]]);
-          hr.push([[width/(2*scl)], [i]]);
+          hl.push([
+               [-width / (2 * scl)],
+               [i]
+          ]);
+          hr.push([
+               [width / (2 * scl)],
+               [i]
+          ]);
      }
+
+     // slider 만들기
+     slider1 = createSlider(0, 1, 0, 0.01);
+
+     // matrix 설정하기
+     myMtx = [
+          [2, -3],
+          [1, 1]
+     ];
+
 }
 
 function draw() {
@@ -35,7 +59,7 @@ function draw() {
 
      // 희미한 grid line 그리기: scale 간격으로.
      plotDimGrid();
-     
+
      // 새로운 grid line 그리기. 이 grid는 선형변환이 apply 될 것임.
      plotNewGrid(vu, vd, hl, hr);
      noLoop();
@@ -45,7 +69,8 @@ function draw() {
 
 function plotDimGrid() {
      push();
-     translate(width/2, height/2);
+     translate(width / 2, height / 2);
+     scale(1, -1); // 결과는 같아 보이지만, 의도한 것이 문제없이 표현되도록...
      stroke(100);
      strokeWeight(0.5);
      for (let i = -floor(height / scl) / 2; i < floor(height / scl) / 2; i++) {
@@ -66,17 +91,17 @@ function plotNewGrid(vu, vd, hl, hr) {
 
      push();
      translate(width / 2, height / 2);
-
+     scale(1, -1); // 결과는 같아 보이지만, 의도한 것이 문제없이 표현되도록...
      // 변환 적용할 lines 그리기
      stroke(109, 155, 222);
      strokeWeight(1);
-     
+
      // vertical line
      for (let i = 0; i < vu.length; i++) {
           line(vu[i][0] * scl, vu[i][1] * scl, vd[i][0] * scl, vd[i][1] * scl);
      }
      // horizontal line
-     for (let i = 0; i< hl.length; i++){
+     for (let i = 0; i < hl.length; i++) {
           line(hl[i][0] * scl, hl[i][1] * scl, hr[i][0] * scl, hr[i][1] * scl);
      }
      // plotting vertical and horizontal lines in the center
