e21d1bd72a686dd6fb7c25cf4772f20b08c48a45
angeloyeo <angeloyeo@gmail.com>
2019-07-16 12:09:58 +0900

    -

--- p5/Matrix_as_a_linear_transformation/transformation1/sketch.js
+++ p5/Matrix_as_a_linear_transformation/transformation1/sketch.js
@@ -12,6 +12,7 @@ let myMtx = [];
 
 function setup() {
      // createCanvas(windowWidth - 20, windowHeight - 20);
+     // TODO: window size에 맞춰서 animation을 만들 수 없을까?
      createCanvas(800, 400);
      scl = floor(height / 8); // height가 낮다보니 height에 scale을 맞추는게 좋아보임.
 
