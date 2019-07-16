05501e13a6c708668210c10ae812e42085faed16
angeloyeo <angeloyeo@gmail.com>
2019-07-16 10:24:48 +0900

    math.js 추가

--- p5/Matrix_as_a_linear_transformation/transformation1/sketch.js
+++ p5/Matrix_as_a_linear_transformation/transformation1/sketch.js
@@ -111,18 +111,3 @@ function plotNewGrid(vu, vd, hl, hr) {
      line(0, -height / 2, 0, height / 2);
      pop();
 }
-
-function multiplyMatrices(m1, m2) {
-     var result = [];
-     for (var i = 0; i < m1.length; i++) {
-          result[i] = [];
-          for (var j = 0; j < m2[0].length; j++) {
-               var sum = 0;
-               for (var k = 0; k < m1[0].length; k++) {
-                    sum += m1[i][k] * m2[k][j];
-               }
-               result[i][j] = sum;
-          }
-     }
-     return result;
-}
