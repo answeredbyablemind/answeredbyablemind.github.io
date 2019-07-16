6f0f486f364ee86b07e136199a8e8482f05dc86e
angeloyeo <angeloyeo@gmail.com>
2019-07-16 13:56:36 +0900

    -

--- p5/Matrix_as_a_linear_transformation/transformation1/sketch.js
+++ p5/Matrix_as_a_linear_transformation/transformation1/sketch.js
@@ -11,9 +11,9 @@ let slider1;
 let myMtx = [];
 
 function setup() {
-     createCanvas(windowWidth - 20, windowHeight - 20);
+     // createCanvas(windowWidth - 20, windowHeight - 20);
      // TODO: window size에 맞춰서 animation을 만들 수 없을까?
-     // createCanvas(800, 400);    
+     createCanvas(600, 480);    
      scl = (height / 8); // height가 낮다보니 height에 scale을 맞추는게 좋아보임.
 
      // 각 아래의 내용을 vu, vd, hl, hr에 맞출 수 있게... 벡터화 하고자 함.
