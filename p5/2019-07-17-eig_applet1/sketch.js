1a851d0afc5d8ccb6cb477ad721e0469d12181b5
Donghoon Yeo <angeloyeo@gmail.com>
2019-07-25 13:26:43 +0900

    Update sketch.js

--- p5/2019-07-17-eig_applet1/sketch.js
+++ p5/2019-07-17-eig_applet1/sketch.js
@@ -8,7 +8,7 @@ let t;
 function setup() {
      // createCanvas(windowWidth - 20, windowHeight - 20);
      // TODO: window size에 맞춰서 animation을 만들 수 없을까?
-     createCanvas(400, 400);
+     createCanvas(windowWidth-20, windowWidth-20);
      scl = (height / 8); // height가 낮다보니 height에 scale을 맞추는게 좋아보임.
 
 
