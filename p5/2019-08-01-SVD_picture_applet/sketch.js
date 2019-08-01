e0a9790a3e9887522258da7c71b31647edcee685
angeloyeo <angeloyeo@gmail.com>
2019-08-01 20:58:53 +0900

    -

---
+++ p5/2019-08-01-SVD_picture_applet/sketch.js
@@ -0,0 +1,12 @@
+function preload(){
+     img = loadImage('lena.png')
+}
+
+function setup() {
+     createCanvas(windowWidth - 20, windowWidth - 20);
+     image(img, 0, 0)
+}
+
+function draw() {
+     background(0);
+}
