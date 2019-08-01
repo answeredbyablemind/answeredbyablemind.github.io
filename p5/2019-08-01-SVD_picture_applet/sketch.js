39992d4db5eff69d5464a2b9d9065f5d52d443cd
angeloyeo <angeloyeo@gmail.com>
2019-08-02 07:37:25 +0900

    -

--- p5/2019-08-01-SVD_picture_applet/sketch.js
+++ p5/2019-08-01-SVD_picture_applet/sketch.js
@@ -1,14 +1,27 @@
 let img;
+let X = [];
+let temp = []
 
-function preload(){
-     img = loadImage('lena.png')
+function preload() {
+     img = loadImage('https://raw.githubusercontent.com/angeloyeo/angeloyeo.github.io/master/p5/2019-08-01-SVD_picture_applet/lena.png')
 }
 
 function setup() {
      createCanvas(windowWidth - 20, windowWidth - 20);
-     image(img, 0, 0)
+     img.loadPixels();
+     let ii=0
+     for(let i=0;i<img.width;i++){
+          X[i]=[]
+          for(let j=0;j<img.height;j++){
+               X[i][j] = img.pixels[ii]
+               ii++
+          }
+     }
 }
 
 function draw() {
-     background(0);
+     // background(0);
+
+
+
 }
