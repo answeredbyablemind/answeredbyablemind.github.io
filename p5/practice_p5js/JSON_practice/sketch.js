e5bfa3f35f9d92b2573d063be65d0a3bc63b8113
angeloyeo <angeloyeo@gmail.com>
2019-08-03 07:37:01 +0900

    -

---
+++ p5/practice_p5js/JSON_practice/sketch.js
@@ -0,0 +1,14 @@
+var flower
+
+function setup() {
+  // put setup code here
+  flower  = {
+    name: "sunflower",
+    col: color(200, 220, 0) 
+  }
+  
+}
+
+function draw() {
+  // put drawing code here
+}
