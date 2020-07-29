53b372f90d3ed9c0a0d6bb3fc6cda871ac29ab54
angeloyeo <angeloyeo@gmail.com>
2020-07-29 15:05:16 +0900

    un deux tois 업데이트

--- p5/practice_p5js/un_deux_trois/sketch.js
+++ p5/practice_p5js/un_deux_trois/sketch.js
@@ -9,7 +9,7 @@ function setup() {
      myButton.position(0, height)
      myButton.mousePressed(buttonPressed)
 
-     aThirdOfHeight = width / 3
+     aThirdOfHeight = height / 3
 
 }
 
@@ -21,7 +21,18 @@ function draw() {
      background(255);
      strokeCap(ROUND)
      strokeWeight(4)
-     my_draw(0,0,10,10,[0,0])
+     for(var y = step; y < height - step; y += step){
+          for(var x = step; x < width - step; x += step){
+               if (y < aThirdOfHeight){
+                    my_draw(x, y, step, step, [0.5])
+               } else if (y < aThirdOfHeight*2){
+                    my_draw(x, y, step, step, [0.2, 0.8])
+               } else {
+                    my_draw(x, y, step, step, [0.1, 0.5, 0.9])
+               }
+               
+          }
+     }
 
      
      noLoop()
@@ -31,8 +42,11 @@ function draw() {
 
 function my_draw(x, y, my_width, my_height, positions){
      for (var i = 0; i <= positions.length; i++){
-          line(x + positions[i] * my_width, y, x + positions[i] * my_width, my_height)
+          push()
+          translate(x + my_width/2, y + my_height/2)
+          rotate(Math.random() * 5)
+          translate(-my_width/2, -my_height/2)
+          line(positions[i] * my_width, 0, positions[i] * my_width, my_height)
+          pop()
      }
-     
-
 }
