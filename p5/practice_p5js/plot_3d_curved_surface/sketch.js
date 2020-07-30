7924b7d9635f32e94b63e1b4f89fe8ebaa8e9637
angeloyeo <angeloyeo@gmail.com>
2020-07-30 10:32:13 +0900

    p5js / curved surface 3D로 그리기 연습

--- p5/practice_p5js/plot_3d_curved_surface/sketch.js
+++ p5/practice_p5js/plot_3d_curved_surface/sketch.js
@@ -2,13 +2,22 @@ var f = [[]]
 var scl
 var x = []
 var y = []
-let cameraLocation;
+var tex
+
+var delta_x
+var delta_y
+var box_height
+var z_axis_ratio = 2
+
+var delta_x_slider
+var delta_y_slider
 
 function setup() {
      createCanvas(320, 320, WEBGL);
+
      setAttributes('antialias', true)
      // my function
-     // z = sqrt(4- x^2- y^2)
+     // z = 4- x^2- y^2
 
      x = linspace(-2, 2, 20)
      y = linspace(-2, 2, 20)
@@ -23,7 +32,19 @@ function setup() {
      }
 
      scl = width / 8
-     cameraLocation = createVector(0, 0, (height/2.0) / tan(PI*30.0 / 180.0));
+
+     // 글씨 쓰기
+     tex = createP()
+     tex.style('font-size', '15px')
+     tex.position(width/4, 0)
+     katex.render('f(x,y) = 4 - x^2 - y^2', tex.elt)
+
+     delta_x_slider = createSlider(0.1, 2, 0.1, 0.05)
+     delta_x_slider.position(0, height)
+     
+     delta_y_slider = createSlider(0.1, 2, 0.1, 0.05)
+     delta_y_slider.position(width/2, height)
+
 
 }
 
@@ -31,26 +52,30 @@ function setup() {
 function draw() {
      background(255);
      rotateX(PI/4)
-     ambientMaterial(10)
+     delta_x = delta_x_slider.value()
+     delta_y = delta_y_slider.value()
+
+     plotAxes()
+
+     plotBoxes()
+
+     orbitControl()
+     my_color = color(100, 50, 100);
+     my_color.setAlpha(50)
+     fill(my_color)
+     // stroke(0)
      noStroke()
+
      for(var j = 0; j < y.length-1; j++){
-          beginShape()
+          beginShape(TESS)
           for(var i = 0; i < x.length-1; i++){
-               vertex(x[i] * scl, y[j] * scl, f[i][j] * scl / 5)
-               vertex(x[i] * scl, y[j+1] * scl, f[i][j+1] * scl / 5)
-               vertex(x[i+1] * scl, y[j+1] * scl,f[i+1][j+1] * scl / 5)
-               vertex(x[i+1] * scl, y[j] * scl,f[i+1][j] * scl / 5)
+               vertex(x[i] * scl, y[j] * scl, f[i][j] * scl / z_axis_ratio)
+               vertex(x[i] * scl, y[j+1] * scl, f[i][j+1] * scl / z_axis_ratio)
+               vertex(x[i+1] * scl, y[j+1] * scl, f[i+1][j+1] * scl / z_axis_ratio)
+               vertex(x[i+1] * scl, y[j] * scl, f[i+1][j] * scl / z_axis_ratio)
           }
           endShape()
      }
-
-
-
-     updateCameraLocation();
-     // console logging camera location helps to conceptialize what's happening
-     // console.log(`Camera: x:${cameraLocation.x}, y:${cameraLocation.y}, z:${cameraLocation.z}`)
-     camera(cameraLocation.x, cameraLocation.y, cameraLocation.z, 0, 0, 0, 0, 1, 0);
-     // noLoop()
 
 }
 
@@ -65,9 +90,47 @@ function linspace(stt, end, steps){
      return res
 }
 
-function updateCameraLocation() {
-     if (keyIsDown(LEFT_ARROW)) cameraLocation.x -= 5;
-     if (keyIsDown(RIGHT_ARROW)) cameraLocation.x += 5;
-     if (keyIsDown(UP_ARROW)) cameraLocation.z -= 5;
-     if (keyIsDown(DOWN_ARROW)) cameraLocation.z += 5;
-   }
+function plotAxes(){
+     let stt = -3
+     let end = 3
+
+     push()
+     
+     stroke(0)
+     // x axis
+     line(stt * scl, 0, 0, end * scl, 0, 0)
+     // y axis
+     line(0, stt * scl, 0, 0, end * scl, 0)
+     // z axis
+     // line(0, 0, -3 * scl, 0, 0, 3 * scl)
+
+     // xtick 그려주기
+     for(let i = stt; i<=end; i++){
+          line(i * scl, -0.1 * scl, 0, i * scl, 0.1 * scl, 0)
+     }
+     // ytick 그려주기
+     for(let i = stt; i<=end; i++){
+          line(-0.1 * scl, i * scl, 0, 0.1 * scl, i * scl, 0)
+     }
+     pop()
+}
+function plotBoxes(){
+     for(var i = -1; i <= 1; i+=delta_x){
+          for(var j = -1; j <= 1; j+=delta_y){
+               let box_height_arr = [
+                    box_height1 = 4 - (i - delta_x/2)**2 - (j - delta_y/2)**2, 
+                    box_height2 = 4 - (i + delta_x/2)**2 - (j - delta_y/2)**2, 
+                    box_height3 = 4 - (i - delta_x/2)**2 - (j + delta_y/2)**2, 
+                    box_height4 = 4 - (i + delta_x/2)**2 - (j + delta_y/2)**2]
+               box_height = min(box_height_arr)
+
+               push()
+               translate(i * scl, j * scl, box_height/2 * scl / z_axis_ratio)
+               fill(255)
+               stroke(0)
+               box(delta_x * scl, delta_y * scl, box_height * scl / z_axis_ratio)
+               pop()
+          }
+     }
+     
+}
