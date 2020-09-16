cb25078a3b6a102702a1027188eab7d411523d70
angeloyeo <angeloyeo@gmail.com>
2020-09-16 14:31:53 +0900

    MCMC 작성 시작

---
+++ pics/2020-09-16-MCMC/pics.m
@@ -0,0 +1,42 @@
+clear; close all; clc;
+
+n_iter = 10000;
+accept = 0;
+reject = 0;
+figure('position',[680, 558, 453, 420],'color','w')
+
+newVid = VideoWriter('pic1', 'MPEG-4'); % New
+newVid.FrameRate = 20;
+newVid.Quality = 100;
+open(newVid);
+
+for i_iter = 1:n_iter
+    x = rand(1) * 2 -1;
+    y = rand(1) * 2 -1;
+    
+    if (x^2 + y^2) <= 1
+        accept = accept + 1;
+        c = 'r';
+    else
+        reject = reject + 1;
+        c = 'b';
+    end
+    plot(x, y,'.', 'color',c);
+    hold on;
+    
+    t = text(0, -0.77557, sprintf('ÃßÁ¤ ³ÐÀÌ: %.4f',4 * accept / i_iter),'fontsize',15,'fontweight','bold','BackgroundColor','w');
+    
+    if rem(i_iter, 100) == 0
+        xlim([-1, 1])
+        ylim([-1, 1])
+        writeVideo(newVid, getframe(gcf))
+        drawnow;
+    end
+    
+    if i_iter < n_iter
+        delete(t);
+    end
+end
+close(newVid)
+
+
