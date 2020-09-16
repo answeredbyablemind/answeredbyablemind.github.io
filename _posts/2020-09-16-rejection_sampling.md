dd39854d8b9c36ae648528600b9efe3c626d51aa
angeloyeo <angeloyeo@gmail.com>
2020-09-16 16:50:38 +0900

    rejection sampling pic1 upload

--- _posts/2020-09-16-rejection_sampling.md
+++ _posts/2020-09-16-rejection_sampling.md
@@ -1,5 +1,5 @@
 ---
-title: rejection sampling
+title: Rejection Sampling
 sidebar:
   nav: docs-ko
 aside:
@@ -7,3 +7,14 @@ aside:
 key: 20200916
 tags: 통계학
 ---
+
+수식을 알고 있는 어떤 확률 밀도 함수가 있다고 하자.
+
+가령 아래와 같은 확률 밀도를 생각해볼 수 있다.
+
+$$f(x) = 0.3\exp\left(-0.2 * x^2\right) + 0.7\exp\left(-0.2 * (x-10)^2\right)$$
+
+이 함수를 plot 해보면 다음과 같은 형태를 가지고 있다는 것을 알 수 있다.
+
+
+
---
+++ pics/2020-09-16-rejection_sampling/pics.m
@@ -0,0 +1,57 @@
+clear; close all; clc;
+
+rng(1)
+n = 50000;
+xx = linspace(-10,20, 1000);
+
+% target distribution
+target = @(x) 0.3*exp(-0.2 * x.^2) + 0.7 * exp(-0.2 * (x - 10).^2);
+
+figure('color','w');
+plot(xx, target(xx),'linewidth',2);
+
+xlabel('$$x$$','interpreter','latex');
+ylabel('$$f(x)$$','interpreter','latex');
+grid on;
+
+title('$$f(x) = 0.3 e^{-0.2x^2} + 0.7 e^{-0.2(x-10)^2}$$','interpreter','latex');
+
+%% uniform을 이용하여 rejection sampling
+% 성능에 크게 차이는 없으나 reject되는 sample 수가 조금 차이가 있음.
+
+pseudo_dist2 = @(x) (x>=-10 * x<20) / 30;
+
+figure;
+plot(xx, target(xx));
+hold on;
+plot(xx, pseudo_dist2(xx)*21); % 21은 30 * 0.7을 얘기함. 30은 출력값이 1인 x의 범주, 0.7은 target의 최고 높이.
+
+x_q = (rand(1, n) - 0.5) * 30 + 5; % 10에서 20사이의 uniform distribution
+
+crits = target(x_q) ./ (pseudo_dist2(x_q) * 21);
+coins = rand(1, length(crits));
+
+x_p_uniform = x_q(coins<crits);
+
+figure; h = histogram(x_p_uniform,'BinWidth',0.5, 'Normalization','probability');
+hold on; plot(xx, target(xx)/max(target(xx))*max(h.Values))
+
+
+
+%% normal distribution을 이용하여 rejection sampling
+pseudo_dist = @(x, mu, sigma) 1/(sigma*sqrt(2*pi)) * exp(-((x-mu).^2)/(2*sigma^2));
+
+
+figure; 
+plot(xx, target(xx))
+hold on;
+plot(xx, 20 * pseudo_dist(xx, 6.5, 4.5))
+
+x_q = randn(1, n) * 4.5 + 6.5; 
+
+crits = target(x_q) ./ (pseudo_dist(x_q, 6.5, 4.5) * 20);
+coins = rand(1, length(crits));
+
+x_p_normal = x_q(coins<crits);
+figure; h = histogram(x_p_normal,'BinWidth',0.5, 'Normalization','probability');
+hold on; plot(xx, target(xx)/max(target(xx))*max(h.Values))
