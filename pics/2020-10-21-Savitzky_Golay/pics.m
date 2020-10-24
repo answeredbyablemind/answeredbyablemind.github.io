20386eb5365b17db898aa2126433726a6b07ce93
Donghoon Yeo <angeloyeo@gmail.com>
2020-10-24 14:02:37 +0900

    -

--- pics/2020-10-21-Savitzky_Golay/pics.m
+++ pics/2020-10-21-Savitzky_Golay/pics.m
@@ -1,3 +1,43 @@
+clear; close all; clc;
+
+%% Impulse response
+close all;
+t = 0:1:10;
+x = sin(t) + 3*cos(t/3) + 4* sin(t/4);
+stem(t, x,'color','k', 'linewidth', 1.5)
+
+t2 = linspace(-0.5,10.5,100);
+x2 = sin(t2) + 3*cos(t2/3) + 4* sin(t2/4);
+hold on;
+plot(t2, x2, 'r--')
+
+xlabel('n (time sample) (bin)');
+ylabel('x[n]');
+grid on;
+xlim([-0.5 10.5])
+
+%% 신호를 분해하자
+close all;
+
+for i = 1:11
+    figure;
+    hold on;
+    plot(t2, x2, 'r--')
+    
+    xlabel('n (time sample) (bin)');
+    ylabel('x[n]');
+    grid on;
+    xlim([-0.5 10.5])
+    x = sin(t(i)) + 3*cos(t(i)/3) + 4* sin(t(i)/4);
+    stem(t(i), x,'color','k', 'linewidth', 1.5)
+end
+
+
+
+
+
+%% Moving Average
+
 rng(2);
 a = rand(1,20);
 sig = a+0.1*(1:20);
