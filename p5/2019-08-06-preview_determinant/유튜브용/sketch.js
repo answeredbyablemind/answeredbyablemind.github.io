0b5a1eb37ef2bf9643a2357cb4c7ea8b0ecae4f6
Donghoon Yeo <angeloyeo@gmail.com>
2020-10-31 21:26:03 +0900

    행렬식 p5js 유튜브용

--- p5/2019-08-06-preview_determinant/유튜브용/sketch.js
+++ p5/2019-08-06-preview_determinant/유튜브용/sketch.js
@@ -23,7 +23,7 @@ var my_slider
 function setup() {
      // createCanvas(windowWidth - 20, windowHeight - 20);
      // TODO: window size에 맞춰서 animation을 만들 수 없을까?
-     createCanvas(650, 650);
+     createCanvas(450, 450);
      scl = (height / 8); // height가 낮다보니 height에 scale을 맞추는게 좋아보임.
 
      my_slider = createSlider(0, 1, 0, 0)
