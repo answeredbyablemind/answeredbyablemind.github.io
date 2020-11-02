8337a9edfaa1c8fa586264076068f61fd85314a0
angeloyeo <angeloyeo@gmail.com>
2020-11-02 14:36:28 +0900

    complex eigen p5js

--- p5/2020-11-02-complex_eigen/eigen1/sketch.js
+++ p5/2020-11-02-complex_eigen/eigen1/sketch.js
@@ -3,46 +3,57 @@ let A = [],
      Ax = [];
 
 let scl;
-let extraCanvas;
-// var myP;
+var slider1
 
 function setup() {
      createCanvas(350, 350);
-     extraCanvas = createGraphics(350, 350);
      // myP = createP('$$\\begin{bmatrix}0.25 & 0.75\\\\ 1 & 0.5\\end{bmatrix}$$');
      // myP.style('color','white')
      
-     A = math.matrix([[0.25, 0.75], [1, 0.5]])
-     // A = math.matrix([[2, 1], [1, 2]])
-     let xx = random(1) * 2 - 1;
-     x = math.matrix([
-          [xx],
-          [sqrt(1 - xx * xx)]
-     ])
-     scl = width / 4;
-     extraCanvas.background(0)
-     
-     slider1 = createSlider(0, 2*PI + 0.01, 0, 0.01);
+     slider1 = createSlider(0, 2*PI, 0, 0);
      slider1.position(0, height + 10)
-
+     
+     scl = width / 3;
 }
 
 function draw() {
      background(0);
-     x = math.matrix([[cos(slider1.value())],[sin(slider1.value())]])
-     image(extraCanvas, 0, 0)
+     x = math.matrix([
+          [cos(slider1.value())],
+          [sin(slider1.value())]
+     ])
+     y = math.matrix([
+          [-sin(slider1.value())],
+          [cos(slider1.value())]
+     ])
      plotAxis();
-     Ax = math.multiply(A, x);
 
-     drawDots();
-     drawArrow(0, 0, x._data[0][0], x._data[1][0], 66, 135, 245)
-     drawArrow(0, 0, Ax._data[0][0], Ax._data[1][0], 245, 135, 66)
+     drawArrow(0, 0, x._data[0][0], x._data[1][0], 245, 135, 66)
+     drawArrow(0, 0, y._data[0][0], y._data[1][0], 0, 114, 189)
      textOnArrow();
-     fill(255);
-     textAlign(CENTER);
-     textSize(12);
-     text('x와 Ax를 평행하게 만들어보세요.', width/2, height * 0.875)
 
+     push()
+     fill(255)
+     // stroke(255,0 ,0)
+     translate(width/2, height/2)
+     scale(1, -1)
+     let arc_end = 0
+
+     if (slider1.value() == 0){
+          arc_end = 0.0001
+     } else {
+          arc_end = slider1.value()
+     }
+     arc(1.25*scl, 1.25*scl, 0.25 * scl, 0.25 * scl, 0, arc_end, PIE)
+     
+     translate(1.25 * scl, 1.25 * scl)
+     
+     stroke(255, 0, 0)
+     strokeWeight(2)
+     line(0, 0, 0.25 / 2 * scl * cos(arc_end), 0.25 / 2 * scl * sin(arc_end))
+     pop()
+
+     fill(255)
      textSize(12)
      textAlign(RIGHT)
      text('(c) 공돌이의 수학정리노트', width * 0.95, height * 0.95)
@@ -55,14 +66,19 @@ function textOnArrow() {
      // scale(1, -1)
      fill(255);
      textSize(13);
-     text('x', x._data[0][0] * 0.5 * scl, -x._data[1][0] * 0.5 * scl);
-     text('Ax', Ax._data[0][0] * 0.5 * scl, -Ax._data[1][0] * 0.5 * scl);
+     text('C1', y._data[0][0] * 0.5 * scl + 20, -y._data[1][0] * 0.5 * scl)
+     text('C2', x._data[0][0] * 0.5 * scl, -x._data[1][0] * 0.5 * scl -5)
+
+     text('1', 1 * scl + 15, 15)
+     text('i', 20, - 1 * scl)
+     text('-1', -1 * scl + 15, 15)
+     text('-i', 20, 1 * scl)
      pop();
 }
 
 function plotAxis() {
      push();
-     stroke(200);
+     stroke(255);
      strokeWeight(2);
      line(width / 2, 0, width / 2, height);
      line(0, height / 2, width, height / 2);
@@ -74,11 +90,11 @@ function plotAxis() {
      stroke(150);
      strokeWeight(0.5);
      // vertical lines
-     for (let i = -1; i <= 1; i++) {
+     for (let i = -2; i <= 2; i++) {
           line(i * scl, -height / 2, i * scl, height / 2)
      }
 
-     for (let i = -1; i <= 1; i++) {
+     for (let i = -2; i <= 2; i++) {
           line(-width / 2, i * scl, width / 2, i * scl)
      }
 
@@ -90,7 +106,7 @@ function drawArrow(x1, y1, x2, y2, c1, c2, c3) {
      push();
      translate(width / 2, height / 2)
      scale(1, -1)
-     strokeWeight(2)
+     strokeWeight(4)
      stroke(c1, c2, c3);
      fill(c1, c2, c3)
      line(x1 * scl, y1 * scl, x2 * scl, y2 * scl); //draw a line beetween the vertices
@@ -104,16 +120,4 @@ function drawArrow(x1, y1, x2, y2, c1, c2, c3) {
           offset * 0.5, offset,
           0, 0); //draws the arrow point as a triangle
      pop();
-}
-
-function drawDots() {
-     extraCanvas.push();
-     extraCanvas.noStroke();
-     extraCanvas.translate(width / 2, height / 2);
-     extraCanvas.scale(1, -1)
-     extraCanvas.fill(66, 135, 245)
-     extraCanvas.ellipse(x._data[0][0] * scl, x._data[1][0] * scl, 5)
-     extraCanvas.fill(245, 135, 66)
-     extraCanvas.ellipse(Ax._data[0][0] * scl, Ax._data[1][0] * scl, 5)
-     extraCanvas.pop();
 }
