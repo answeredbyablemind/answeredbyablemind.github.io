1556fefe90b91bae58728b87b5f99663647054b5
angeloyeo <angeloyeo@gmail.com>
2020-11-06 15:06:17 +0900

    DFT pics

--- pics/2019-07-14-Freq_Sampling/pics.m
+++ pics/2019-07-14-Freq_Sampling/pics.m
@@ -1,5 +1,34 @@
 clear; close all; clc;
 
+addpath('D:\angeloyeo.github.io\MATLAB\다른사람참고code\Annotate-v1.2.1\Annotate\');
+addpath('C:\angeloyeo.github.io\MATLAB\다른사람참고code\Annotate-v1.2.1\Annotate\');
+
+%% 원 위의 2pi/8 만큼 CW 회전한 곳의 점 
+
+figure('color','w','position',[680, 317, 500, 413]);
+hold on;
+set(gca,'Visible','off')
+xlim([-1.5, 1.5])
+ylim([-1.5, 1.5])
+
+Annotate(gca,'arrow', [-1.5, 1.5], [0, 0]);
+Annotate(gca,'arrow', [0, 0], [-1.5, 1.5]);
+
+theta = linspace(0, 2*pi, 100);
+plot(cos(theta), sin(theta),'k');
+plot(cos(-2*pi/8), sin(-2*pi/8),'o','markerfacecolor','k','markeredgecolor','none');
+plot([0, cos(-2*pi/8)], [0, sin(-2*pi/8)],'k','linewidth',2)
+
+axis square
+
+theta = linspace(-2*pi/8, 0, 20);
+plot(0.3 * cos(theta), 0.3 * sin(theta),'k');
+
+for i = [0, 2:7]
+    plot(cos(-2*pi/8 * i), sin(-2*pi/8 * i),'r*','markersize',8);
+end
+
+
 %% cosine
 fs = 500;
 t = 0:1/fs:1;
