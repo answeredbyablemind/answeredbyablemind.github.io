a14ae1dc4a63724855322dd2a74fd733718f2872
angeloyeo <angeloyeo@gmail.com>
2020-11-09 10:05:44 +0900

    -

---
+++ pics/2020-11-08-linear_algebra_and_Fourier_transform/pics_for_FFT.m
@@ -0,0 +1,38 @@
+clear; close all; clc;
+
+Fs = 1000;
+T = 1/Fs;
+L = 1500;
+t = 0:1/Fs:(L-1)/Fs;
+
+S = 0.7*sin(2*pi*50*t) + sin(2*pi*120*t);
+figure;
+subplot(2,1,1);
+plot(t, S)
+xlabel('time (s)');
+ylabel('signal s(t)')
+grid on;
+title('전체 시간의 신호 파형');
+
+subplot(2,1,2);
+plot(t, S)
+xlabel('time (s)');
+ylabel('signal s(t)')
+grid on;
+xlim([0, 0.3])
+title('신호의 일부 구간을 확대한 그림');
+%% FFT 
+Y = fft(S);
+P2 = abs(Y/L);
+P1 = P2(1:L/2+1);
+P1(2:end-1) = 2*P1(2:end-1);
+
+f = Fs*(0:(L/2))/L;
+
+figure;
+plot(f,P1) 
+title('Single-Sided Amplitude Spectrum of S(t)')
+xlabel('f (Hz)')
+ylabel('|P1(f)|')
+grid on;
+% xlim([0, 150])
