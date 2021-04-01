f2f492d4e073131d0ffee4a39bedc5c0b599b0fb
angeloyeo <angeloyeo@gmail.com>
2021-04-01 12:27:52 +0900

    -

--- pics/2021-02-07-k_means/pics.m
+++ pics/2021-02-07-k_means/pics.m
@@ -1,7 +1,7 @@
 clear; close all; clc;
 
 %% 클러스터링 문제 설명 위한 그림
-
+% pic 1
 S1 = [0.2, 1;1, 0.3] /10;
 S2 = [0.3, -1; -1, 0.2] /10;
 n = 500;
@@ -15,3 +15,28 @@ plot(clust2(:,1), clust2(:,2),'o','markerfacecolor',ones(1,3) * 0.4,'markeredgec
 grid on;
 xlabel('x');
 ylabel('y');
+
+%% pic 2
+% pic 1
+my_color = lines(2);
+
+figure;
+plot(clust1(:,1), clust1(:,2),'o','markerfacecolor',my_color(1,:),'markeredgecolor','none')
+hold on;
+plot(clust2(:,1), clust2(:,2),'o','markerfacecolor',my_color(2,:),'markeredgecolor','none');
+grid on;
+xlabel('x');
+ylabel('y');
+
+%% 
+
+clust = [clust1; clust2];
+[idx, centroids] = kmeans(clust ,2);
+
+figure;
+plot(clust(idx==1,1), clust(idx==1,2),'o');
+hold on;
+plot(clust(idx==2,1), clust(idx==2,2),'o');
+
+plot(centroids(1,1), centroids(1,2),'kx','linewidth',3,'markersize',15);
+plot(centroids(2,1), centroids(2,2),'kx','linewidth',3,'markersize',15);
