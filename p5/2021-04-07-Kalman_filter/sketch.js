58e56d2f76e4cc6ea095cc55a15ff1838c516a4f
angeloyeo <angeloyeo@gmail.com>
2021-04-07 09:54:41 +0900

    Kalman filter P5 first commit

---
+++ p5/2021-04-07-Kalman_filter/sketch.js
@@ -0,0 +1,13 @@
+let scl;
+
+function setup() {
+     // createCanvas(windowWidth - 20, windowHeight - 20);
+     // TODO: window size에 맞춰서 animation을 만들 수 없을까?
+     createCanvas(300, 240);
+     scl = (height / 8); // height가 낮다보니 height에 scale을 맞추는게 좋아보임
+}
+
+function draw() {
+     background(0);
+
+}
