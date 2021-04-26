bbe662775c8ce23a1366f730edfa7217778f331a
angeloyeo <angeloyeo@gmail.com>
2021-04-26 10:38:07 +0900

    -

--- pics/2021-04-26-Poisson_distribution/pics.m
+++ pics/2021-04-26-Poisson_distribution/pics.m
@@ -4,10 +4,10 @@ clear; close all; clc;
 
 lambda = 10;
 
-f_binomial = @(x, n, p) factorial(n)./(factorial(x).*factorial(n-x)) .* p.^x .* (1-p).^(n-x);
+% f_binomial = @(x, n, p) factorial(n)./(factorial(x).*factorial(n-x)) .* p.^x .* (1-p).^(n-x);
 f_poisson = @(n, lambda) lambda.^n * exp(-lambda)./factorial(n);
 
 figure;
 stem(1:100, f_poisson(1:100, 10))
-hold on;
-stem(0:540, f_binomial(0:540, 540, 0.003))
+% hold on;
+% stem(0:540, f_binomial(0:540, 540, 0.003))
