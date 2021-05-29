fb3125e6f2a26c7d21c41bab7c71358495789193
Donghoon Yeo <angeloyeo@gmail.com>
2021-05-29 12:48:56 +0900

    BVP 작성 중

--- _posts/2021-05-28-Boundary_Value_Problem.md
+++ _posts/2021-05-28-Boundary_Value_Problem.md
@@ -14,12 +14,23 @@ tags: 미분방정식
 
 * [방향장과 오일러 방법](https://angeloyeo.github.io/2021/04/30/direction_fields.html)
 * [미분방정식과 자연상수 e](https://angeloyeo.github.io/2021/05/05/ODE_and_natural_number_e.html)
-
+* [2계 선형미분방정식의 해법](https://angeloyeo.github.io/2021/05/27/second_order_ODE.html)
+  
 # Motivation
 
 로켓을 발사한다고 생각해보자.
 
 Boundary Value Problem은 Boundary Condition에 따라 Solution에 미치는 영향이 큼.
 
-BVP의 유일해 조건?
+# 초기값 문제와 경계값 문제의 비교
+
+$$\frac{dy}{dt}=\frac{d^2x}{dt^2} = 4\frac{dx}{dt}-3x(t) % 식 (15)$$
+
+$$\Rightarrow \begin{bmatrix}dx/dt \\ dy/dt\end{bmatrix} = \begin{bmatrix}0 & 1 \\ -3 & 4\end{bmatrix}\begin{bmatrix}x \\ y\end{bmatrix} % 식 (17)$$
+
+$$\text{1) 초기 조건: }x(0) = 2,\quad x'(0) = 2$$
+
+$$\text{2) 경계 조건: }x(1) = 2,\quad x(3) = 7$$
+
+
 
--- pics/2021-05-28-Boundary_Value_Proglem/pics.m
+++ pics/2021-05-28-Boundary_Value_Proglem/pics.m
@@ -5,15 +5,60 @@ fn = fullfile(fn, '..');
 fn = [fn '\2021-05-11-modeling_with_systems'];
 addpath(fn);
 addpath('D:\angeloyeo.github.io\pics\');
+addpath('C:\angeloyeo.github.io\pics\');
 
-%% the ODE to solve
 
 global A BoundaryCondition
 
 A = [0, 1;-3, 4];
 % BoundaryCondition = [1, 1.5]; BoundaryCondition_Time = [0, 1.5];
 % BoundaryCondition = [1, 3]; BoundaryCondition_Time = [0, 2];
-BoundaryCondition = [-2, -7]; BoundaryCondition_Time = [1, 3];
+BoundaryCondition = [2, 7]; BoundaryCondition_Time = [1, 3];
+
+%% phase plane of the ODE to solve (IVP)
+
+fun_dxdt = @(x,y) A(1,1) * x + A(1,2) * y;
+fun_dydt = @(x,y) A(2,1) * x + A(2,2) * y;
+
+xx = linspace(-10,10,20);
+yy = xx;
+
+figure;
+fun_dirfield_system(fun_dxdt, fun_dydt, xx, yy)
+hold on;
+xlim([xx(1), xx(end)])
+ylim(xlim)
+
+posX = 2; posY = 2;
+
+plot(posX, posY, 'o', 'markerfacecolor','r','markeredgecolor','k')
+text(posX+0.5, posY-1, '@ t = 0',...
+    'HorizontalAlignment','left','backgroundColor','w','edgecolor','k')
+
+%% phase plane of the ODE to solve (BVP)
+
+fun_dxdt = @(x,y) A(1,1) * x + A(1,2) * y;
+fun_dydt = @(x,y) A(2,1) * x + A(2,2) * y;
+
+xx = linspace(-10,10,20);
+yy = xx;
+
+figure;
+fun_dirfield_system(fun_dxdt, fun_dydt, xx, yy)
+hold on;
+xlim([xx(1), xx(end)])
+ylim(xlim)
+
+my_color = lines(2);
+line(ones(1,2) * BoundaryCondition(1), ylim, 'color',my_color(1,:),'linestyle','--','linewidth',2)
+line(ones(1,2) * BoundaryCondition(2), ylim, 'color',my_color(2,:),'linestyle','--','linewidth',2)
+YLIMs = ylim;
+text(BoundaryCondition(1), (YLIMs(2) - YLIMs(1)) * 1.05 + YLIMs(1), ['@ t = ',num2str(BoundaryCondition_Time(1))],...
+    'HorizontalAlignment','center','backgroundColor','w','edgecolor','k')
+text(BoundaryCondition(2), (YLIMs(2) - YLIMs(1)) * 1.05 + YLIMs(1), ['@ t = ',num2str(BoundaryCondition_Time(2))],...
+    'HorizontalAlignment','center','backgroundColor','w','edgecolor','k')
+
+%% the ODE to solve
 
 odefun = @bvpfcn;
 bcfun = @bcfcn;
@@ -24,15 +69,36 @@ sol = bvp4c(odefun, bcfun, solinit);
 fun_dxdt = @(x,y) A(1,1) * x + A(1,2) * y;
 fun_dydt = @(x,y) A(2,1) * x + A(2,2) * y;
 
-xx = linspace(-10,10,30);
+xx = linspace(-10,10,20);
 yy = xx;
 
 figure;
 fun_dirfield_system(fun_dxdt, fun_dydt, xx, yy)
 hold on;
-plot(sol.y(1,:), sol.y(2,:),'linewidth',2)
+plot(sol.y(1,:), sol.y(2,:),'linewidth',2,'color','k')
 xlim([xx(1), xx(end)])
 ylim(xlim)
-% 
-% figure;
-% plot(sol.y(1,:), sol.y(2,:),'linewidth',2)
+
+my_color = lines(3);
+line(ones(1,2) * BoundaryCondition(1), ylim, 'color',my_color(1,:),'linestyle','--','linewidth',2)
+line(ones(1,2) * BoundaryCondition(2), ylim, 'color',my_color(2,:),'linestyle','--','linewidth',2)
+YLIMs = ylim;
+text(BoundaryCondition(1), (YLIMs(2) - YLIMs(1)) * 1.05 + YLIMs(1), ['@ t = ',num2str(BoundaryCondition_Time(1))],...
+    'HorizontalAlignment','center','backgroundColor','w','edgecolor','k')
+text(BoundaryCondition(2), (YLIMs(2) - YLIMs(1)) * 1.05 + YLIMs(1), ['@ t = ',num2str(BoundaryCondition_Time(2))],...
+    'HorizontalAlignment','center','backgroundColor','w','edgecolor','k')
+
+time2see = BoundaryCondition_Time(1):BoundaryCondition_Time(2);
+
+for i_time = 1:length(time2see)
+    [~,idx] = min(abs(sol.x - time2see(i_time)));
+    plot(sol.y(1, idx), sol.y(2, idx),'o','markerfacecolor','y', 'markeredgecolor','k');
+    text(sol.y(1, idx)-2.8, sol.y(2, idx)+1.2, ['@ t = ', num2str(time2see(i_time))],'BackgroundColor','w','EdgeColor','k')
+end
+%     plot(sol.y(1, idx), sol.y(2, idx),'o','markerfacecolor','k', 'markeredgecolor','k');
+
+    
+    
+    
+    
+    
