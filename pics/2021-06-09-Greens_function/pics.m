cd35db5420ba31c2969e09193d92e104a6663b47
angeloyeo <angeloyeo@gmail.com>
2021-06-10 21:17:03 +0900

    -

--- pics/2021-06-09-Greens_function/pics.m
+++ pics/2021-06-09-Greens_function/pics.m
@@ -7,18 +7,18 @@ p2 = [3, -2, -5];
 yy1 = polyval(poly(p1), xx);
 yy2 = polyval(poly(p2), xx);
 
-my_color = lines(3);
+my_color = lines(4);
 %% 표시할 부분 선정
 
 highlight = 65:70;
 
 figure;
-plot(xx, yy1, 'color',my_color(1,:),'linewidth',3);
+plot(xx, yy1, 'color',my_color(1,:),'linewidth',5);
 hold on;
-plot(xx(highlight), yy1(highlight),'color','r','linewidth',5)
+plot(xx(highlight), yy1(highlight),'color','r','linewidth',8)
 set(gca,'visible','off')
 figure;
-plot(xx, yy2, 'color',my_color(3,:),'linewidth',3);
+plot(xx, yy2, 'color',my_color(1,:),'linewidth',5);
 hold on;
-plot(xx(highlight), yy2(highlight),'color','r','linewidth',5)
+plot(xx(highlight), yy2(highlight),'color','r','linewidth',8)
 set(gca,'visible','off')
