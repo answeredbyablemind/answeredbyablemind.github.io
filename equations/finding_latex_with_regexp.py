cfe1c61c79f37d6125048f15eaba0f4c01c34ef1
Donghoon Yeo <angeloyeo@gmail.com>
2022-07-18 07:07:17 +0900

    3

--- equations/finding_latex_with_regexp.py
+++ equations/finding_latex_with_regexp.py
@@ -29,8 +29,29 @@ mo_newline = regex_newline.findall(my_text)
 is_newline = find_newline(mo_all, mo_newline)
 
 #%%
-import sympy as sp
 
-sp.init_printing()
+import matplotlib.pyplot as plt
+import io
+from PIL import Image, ImageChops
 
-sp.preview('x**2')
+white = (255, 255, 255, 255)
+
+def latex_to_img(tex):
+    buf = io.BytesIO()
+    plt.rc('text', usetex=True)
+    plt.rc('font', family='serif')
+    plt.axis('off')
+    plt.text(0.05, 0.5, f'$${tex}$$', size=40)
+    plt.savefig(buf, format='png')
+    plt.close()
+
+    im = Image.open(buf)
+    bg = Image.new(im.mode, im.size, white)
+    diff = ImageChops.difference(im, bg)
+    diff = ImageChops.add(diff, diff, 2.0, -100)
+    bbox = diff.getbbox()
+    return im.crop(bbox)
+
+latex_to_img(mo_all[1]).save('img.png')
+
+
