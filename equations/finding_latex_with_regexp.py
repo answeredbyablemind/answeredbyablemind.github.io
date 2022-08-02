951760b70ef2056c35154cfb8c8a98db05493af7
Donghoon Yeo <angeloyeo@gmail.com>
2022-08-02 21:05:43 +0900

    -

--- equations/finding_latex_with_regexp.py
+++ equations/finding_latex_with_regexp.py
@@ -35,7 +35,7 @@ filenames = glob.glob(basefolder+'*.md')
 for i in range(len(filenames)):
     filenames[i] = filenames[i][len(basefolder):-3]
     
-filename = filenames[89]
+filename = filenames[93]
 
 f = open(basefolder+filename+extension ,encoding = 'UTF-8')
 my_text = f.read()
