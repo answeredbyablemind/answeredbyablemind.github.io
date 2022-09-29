0db25a4b209a0e103bcf88621672d30775e23390
Donghoon Yeo <angeloyeo@gmail.com>
2022-09-29 16:53:04 +0900

    -

--- pics/2022-09-28-Mahalanobis_distance/pics.m
+++ pics/2022-09-28-Mahalanobis_distance/pics.m
@@ -17,20 +17,20 @@ ylim([-7, 7]);
 % plot(X_p(:,1), X_p(:,2),'o','markerfacecolor',[0.85, 0.325, 0.098], 'markeredgecolor','none')
 
 %% Cholesky 분해를 이용해 시각화하는 방법도 있음
-% R = chol([3,2;2,4]);
-% rng('default')
-% X_new = randn(1000,2) * R;
-% 
-% figure;
-% 
-% plot(X(:,1), X(:,2),'o','markerfacecolor',lines(1),'markeredgecolor','none');
-% xlabel('$$x_1$$','interpreter','latex');
-% ylabel('$$x_2$$','interpreter','latex');
-% grid on;
-% xlim([-7, 7]);
-% ylim([-7, 7]);
-% 
-% S_new = (X_new - mean(X_new))'*(X_new - mean(X_new))/(1000-1);
+R = chol([3,2;2,4]);
+rng('default')
+X_new = randn(1000,2) * R;
+
+figure;
+
+plot(X(:,1), X(:,2),'o','markerfacecolor',lines(1),'markeredgecolor','none');
+xlabel('$$x_1$$','interpreter','latex');
+ylabel('$$x_2$$','interpreter','latex');
+grid on;
+xlim([-7, 7]);
+ylim([-7, 7]);
+
+S_new = (X_new - mean(X_new))'*(X_new - mean(X_new))/(1000-1);
 
 %%
 X = randn(200,2) * [3,2;2,4];
